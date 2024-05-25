e86a8c43e36eb89f1c8c62a93f809bf76fe6cc29
SohailSajid <sohailsajid79@gmail.com>
2024-05-25 20:16:00 +0100

    tf workflow #1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,37 @@
+provider  "azurerm" {
+  features {}
+
+  tenant_id = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name = "ResourceGroup_ai_project"
+  location = "UK South"
 }
 
 resource "azurerm_static_site" "static_site" {
-  name                = var.static_web_app_name
+  name                = "StaticWebApp"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku_tier            = "Free"
+
+  sku_tier = "Free"
+  sku_size = "Free"
+
+  app_location = "./"
+  api_location = "api"
+  output_location = "build"
+
+  auth_settings {
+    enabled = true
+  }
 
   build_properties {
-    app_location             = "./"  # Adjust as necessary
-    api_location             = "api"
-    output_location          = "build"
+    app_location    = "./"
+    api_location    = "api"
+    output_location = "build"
   }
 }
 
