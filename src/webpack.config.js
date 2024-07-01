3a118778e725546173bf36d70a89bfc676f58833
SohailSajid <sohailsajid79@gmail.com>
2024-07-01 17:32:07 +0100

    fix Webpack polyfill issue

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -4,8 +4,9 @@ module.exports = {
   resolve: {
     fallback: {
       path: require.resolve("path-browserify"),
-      // os: require.resolve("os-browserify/browser"),
-      // crypto: require.resolve("crypto-browserify"),
+      path: require.resolve("path-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      crypto: require.resolve("crypto-browserify"),
     },
   },
 };
