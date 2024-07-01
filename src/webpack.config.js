70b9838c536ee7e0dd45fe46d4311140b6c2a722
SohailSajid <sohailsajid79@gmail.com>
2024-07-01 17:44:39 +0100

    fix webpack polyfill issue

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,8 +3,7 @@ const path = require("path");
 module.exports = {
   resolve: {
     fallback: {
-      path: require.resolve("path-browserify"),
-      stream: require.resolve("stream-browserify"),
+      path: false,
     },
   },
   target: "node",
